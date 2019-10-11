a3f5b9e6a290f318dfb397d5d6d674fd400589b9
David Becker <david@beckersite.com>
2019-10-11 12:50:56 -0600

    Add simple functions to run test suites

---
+++ testRunner.js
@@ -0,0 +1,22 @@
+import consoleColors from './utils/consoleColors';
+
+export const describe = ({ description, callback }) => {
+  console.log(consoleColors.FgWhite, description)
+  callback()
+}
+
+export const test = ({ testName, callback }) => {
+  console.log(consoleColors.FgCyan, testName)
+  callback()
+}
+
+export const expect = ({ actual, expected }) => {
+  if (expected === actual) {
+    console.log(consoleColors.FgGreen, "Passed")
+  } else {
+    console.log(consoleColors.FgRed, "Failed")
+    console.log(consoleColors.FgYellow, `Expected ${actual} to equal ${expected}`)
+  }
+}
+
+// describe({ description: 'hello' })
--- tests/test.spec.js
+++ tests/test.spec.js
@@ -1,6 +1,23 @@
-const test = () => {
-  let thing = 'a thing'
-  console.log(thing)
-}
+import { describe, test, expect } from '../testRunner'
 
-test()
+describe({ description: 'ParentComponent', callback: () => {
+  test({ testName: 'it should do this thing', callback: () => {
+    expect({ actual: 2, expected: 1 })
+  }})
+
+  test({ testName: 'it should do this thing2', callback: () => {
+    expect({ actual: 3, expected: 2 })
+  }})
+
+  test({ testName: 'it should do this thing3', callback: () => {
+    expect({ actual: 1, expected: 1 })
+  }})
+
+  test({ testName: 'it should do this thing4', callback: () => {
+    expect({ actual: 2, expected: 1 })
+  }})
+
+  test({ testName: 'it should do this thing5', callback: () => {
+    expect({ actual: 5, expected: 1 })
+  }})
+}})
