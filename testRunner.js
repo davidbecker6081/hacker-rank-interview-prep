1f0b71f86a24b16123b5f5f15b7c3a3957b5fcc4
David Becker <david@beckersite.com>
2019-10-13 22:28:15 -0600

    Update test runner texpect method to return object containing equality method

--- testRunner.js
+++ testRunner.js
@@ -1,16 +1,24 @@
 import consoleColors from './utils/consoleColors';
 
-export const describe = ({ description, callback }) => {
+export const describe = (description, callback) => {
   console.log(consoleColors.FgWhite, description)
   callback()
 }
 
-export const test = ({ testName, callback }) => {
+export const test = (testName, callback) => {
   console.log(consoleColors.FgCyan, testName)
   callback()
 }
 
-export const expect = ({ actual, expected }) => {
+export const expect = (actual) => {
+  return {
+    to: {
+      equal: testEquality.bind(this, actual)
+    }
+  }
+}
+
+const testEquality = (actual, expected) => {
   if (expected === actual) {
     console.log(consoleColors.FgGreen, "Passed")
   } else {
@@ -18,5 +26,3 @@ export const expect = ({ actual, expected }) => {
     console.log(consoleColors.FgYellow, `Expected ${actual} to equal ${expected}`)
   }
 }
-
-// describe({ description: 'hello' })
--- tests/test.spec.js
+++ tests/test.spec.js
@@ -1,23 +1,7 @@
 import { describe, test, expect } from '../testRunner'
 
-describe({ description: 'ParentComponent', callback: () => {
-  test({ testName: 'it should do this thing', callback: () => {
-    expect({ actual: 2, expected: 1 })
-  }})
-
-  test({ testName: 'it should do this thing2', callback: () => {
-    expect({ actual: 3, expected: 2 })
-  }})
-
-  test({ testName: 'it should do this thing3', callback: () => {
-    expect({ actual: 1, expected: 1 })
-  }})
-
-  test({ testName: 'it should do this thing4', callback: () => {
-    expect({ actual: 2, expected: 1 })
-  }})
-
-  test({ testName: 'it should do this thing5', callback: () => {
-    expect({ actual: 5, expected: 1 })
-  }})
-}})
+describe('ParentComponent', () => {
+  test('it should do this thing', () => {
+    expect(2).to.equal(1)
+  })
+})
